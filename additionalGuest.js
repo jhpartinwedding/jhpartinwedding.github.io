3dd72b81730e7e9b5647075e91c5591d2a42b3ef
jhpartinwedding <jhpartinwedding@gmail.com>
2018-02-03 22:31:55 -0500

    limit number of guests

--- additionalGuest.js
+++ additionalGuest.js
@@ -1,19 +1,33 @@
 guestNum = 1;
 
         function addGuest() {
-            var linebreak = document.createElement("br");
-            var linebreak2 = document.createElement("br");
-            var addGuestHeader = document.createElement("h3");
-            addGuestHeader.textContent = "Additional Guest Info";
-            var addGuest = document.createElement("span");
-            addGuest.textContent = "Guest Name: ";
-            var textbox = document.createElement("input");
-            textbox.type = "text";
-            textbox.name = "additionalGuest" + guestNum;
-            guestNum++;
-            document.getElementById("additionalGuest").appendChild(addGuestHeader);
-            document.getElementById("additionalGuest").appendChild(addGuest);
-            document.getElementById("additionalGuest").appendChild(textbox);
-            document.getElementById("additionalGuest").appendChild(linebreak);
-            document.getElementById("additionalGuest").appendChild(linebreak2);
+            if (guestNum <= 3) {
+                console.log(guestNum);
+                var linebreak = document.createElement("br");
+                var linebreak2 = document.createElement("br");
+
+                var addGuestHeader = document.createElement("h3");
+                addGuestHeader.textContent = "Additional Guest Info";
+
+                var addGuest = document.createElement("span");
+                addGuest.textContent = "Guest Name: ";
+
+                var textbox = document.createElement("input");
+                textbox.type = "text";
+                textbox.name = "additionalGuest" + guestNum;
+                guestNum++;
+                
+                document.getElementById("additionalGuest").appendChild(addGuestHeader);
+                document.getElementById("additionalGuest").appendChild(addGuest);
+                document.getElementById("additionalGuest").appendChild(textbox);
+                document.getElementById("additionalGuest").appendChild(linebreak);
+                document.getElementById("additionalGuest").appendChild(linebreak2);
+            } else {
+                console.log(guestNum);
+                var guestError = document.createElement("h3");
+                guestError.textContent = "Sorry, but we are limiting everyone to a maximum of 3 guests!";
+
+                document.getElementById("additionalGuest").appendChild(guestError);
+                document.getElementById("addGuestBtn").disabled = true;
+            }
         }
