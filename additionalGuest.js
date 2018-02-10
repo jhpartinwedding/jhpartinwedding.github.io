dd0ca540e549eb90c49e7b69a5a11d6200eda506
jhpartinwedding <jhpartinwedding@gmail.com>
2018-02-09 22:18:35 -0500

    add additional guest information

--- additionalGuest.js
+++ additionalGuest.js
@@ -5,6 +5,8 @@ guestNum = 1;
                 console.log(guestNum);
                 var linebreak = document.createElement("br");
                 var linebreak2 = document.createElement("br");
+                var linebreak3 = document.createElement("br");
+                var linebreak4 = document.createElement("br");
 
                 var addGuestHeader = document.createElement("h3");
                 addGuestHeader.textContent = "Additional Guest Info";
@@ -15,13 +17,26 @@ guestNum = 1;
                 var textbox = document.createElement("input");
                 textbox.type = "text";
                 textbox.name = "additionalGuest" + guestNum;
+
+                var addGuestDiet = document.createElement("span");
+                addGuestDiet.textContent = "Guest Dietary Needs: ";
+
+                var textboxDiet = document.createElement("input");
+                textboxDiet.type = "text";
+                textboxDiet.name = "guestDiet" + guestNum;
+
                 guestNum++;
-                
+
                 document.getElementById("additionalGuest").appendChild(addGuestHeader);
                 document.getElementById("additionalGuest").appendChild(addGuest);
                 document.getElementById("additionalGuest").appendChild(textbox);
                 document.getElementById("additionalGuest").appendChild(linebreak);
                 document.getElementById("additionalGuest").appendChild(linebreak2);
+
+                document.getElementById("additionalGuest").appendChild(addGuestDiet);
+                document.getElementById("additionalGuest").appendChild(textboxDiet);
+                document.getElementById("additionalGuest").appendChild(linebreak3);
+                document.getElementById("additionalGuest").appendChild(linebreak4);
             } else {
                 console.log(guestNum);
                 var guestError = document.createElement("h3");
