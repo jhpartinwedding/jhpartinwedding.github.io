83ac27236d2e57ab7bf26b0636422b8a3333fe6f
jhpartinwedding <jhpartinwedding@gmail.com>
2018-04-13 11:39:27 -0400

    validate fields

--- additionalGuest.js
+++ additionalGuest.js
@@ -4,56 +4,12 @@ guestNum = 1;
             if (document.getElementById("addGuestBtn").value = "Add Additional Guest") {
                 document.getElementById("addGuestBtn").value = "Undo Add Guest";
             }
-            if (guestNum <= 1) {
-                console.log(guestNum);
-                var linebreak = document.createElement("br");
-                var linebreak2 = document.createElement("br");
-                var linebreak3 = document.createElement("br");
-                var linebreak4 = document.createElement("br");
-
-                var addGuestHeader = document.createElement("h2");
-                addGuestHeader.textContent = "Additional Guest Info";
-
-                var addGuest = document.createElement("span");
-                addGuest.textContent = "Guest Name: ";
-
-                var textbox = document.createElement("input");
-                textbox.required;
-                textbox.type = "text";
-                textbox.name = "additionalGuest" + guestNum;
-
-                var addGuestDiet = document.createElement("span");
-                addGuestDiet.textContent = "Guest Dietary Needs: ";
-
-                var textboxDiet = document.createElement("input");
-                textboxDiet.required;
-                textboxDiet.type = "text";
-                textboxDiet.name = "guestDiet" + guestNum;
-
-                guestNum++;
-
-                document.getElementById("additionalGuest").appendChild(addGuestHeader);
-                document.getElementById("additionalGuest").appendChild(addGuest);
-                document.getElementById("additionalGuest").appendChild(textbox);
-                document.getElementById("additionalGuest").appendChild(linebreak);
-                document.getElementById("additionalGuest").appendChild(linebreak2);
-
-                document.getElementById("additionalGuest").appendChild(addGuestDiet);
-                document.getElementById("additionalGuest").appendChild(textboxDiet);
-                document.getElementById("additionalGuest").appendChild(linebreak3);
-                document.getElementById("additionalGuest").appendChild(linebreak4);
+            
+            var x = document.getElementById("additionalGuest");
+            if (x.className == "hidden") {
+                x.className = "unhidden";
             } else {
-                if (document.getElementById("addGuestBtn").value = "Undo Add Guest") {
-                    document.getElementById("addGuestBtn").value = "Add Additional Guest";
-                    document.getElementById("additionalGuest").innerText = "";
-                    guestNum--;
-                } else {
-                    console.log(guestNum);
-                    var guestError = document.createElement("h3");
-                    guestError.textContent = "Sorry, but we are limiting everyone to a maximum of 1 guest!";
-
-                    document.getElementById("additionalGuest").appendChild(guestError);
-                    document.getElementById("addGuestBtn").disabled = true;
-                }
+                x.className = "hidden";
             }
+            
         }
