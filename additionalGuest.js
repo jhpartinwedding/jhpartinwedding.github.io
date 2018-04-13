674ad311ab0131ce7dbae6a0419102ff2f8ad60a
jhpartinwedding <jhpartinwedding@gmail.com>
2018-04-13 11:48:05 -0400

    minor changes

--- additionalGuest.js
+++ additionalGuest.js
@@ -1,15 +1,15 @@
-guestNum = 1;
-
-        function addGuest() {
-            if (document.getElementById("addGuestBtn").value = "Add Additional Guest") {
-                document.getElementById("addGuestBtn").value = "Undo Add Guest";
-            }
-            
-            var x = document.getElementById("additionalGuest");
-            if (x.className == "hidden") {
-                x.className = "unhidden";
-            } else {
-                x.className = "hidden";
-            }
-            
-        }
+function addGuest() {
+    if (document.getElementById("addGuestBtn").value == "Add Additional Guest") {
+        document.getElementById("addGuestBtn").value = "Undo Add Guest";
+    } else if (document.getElementById("addGuestBtn").value == "Undo Add Guest") {
+        document.getElementById("addGuestBtn").value = "Add Additional Guest";
+    }
+    
+    var x = document.getElementById("additionalGuest");
+    if (x.className == "hidden") {
+        x.className = "unhidden";
+    } else {
+        x.className = "hidden";
+    }
+    
+}
